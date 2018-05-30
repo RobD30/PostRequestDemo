52d84e3da165f2ba8894523db4cd26490c07ed92
RobD30 <rob.dickerson.dev@gmail.com>
2018-05-30 20:16:14 +0000

    Made Holleys page for her lunch

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get('/', function(req, res){
     res.render('home');
 })
 
+app.get('/holley', function(req, res){
+    res.render('holley');
+})
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log('Server started...');
 })
