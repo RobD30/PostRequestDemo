a616aab738d9801758a657cea416ee55a6569733
RobD30 <rob.dickerson.dev@gmail.com>
2018-05-31 19:43:45 +0000

    Added capture and pass of new friend

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 var express = require("express");
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
+
+const friends = ['Krazy', 'Amanda', 'Avery', 'Marshall', 'Dimitar', 'Charmander'];
 
 app.get('/', function(req, res){
     res.render('home');
 })
 
+app.post('/addfriend',function(req, res){
+    var newFriend = req.body.newFriend;
+    friends.push(newFriend);
+    res.redirect('/friends');
+})
+
 app.get('/friends', function(req, res){
-    var friends = ['Krazy', 'Amanda', 'Avery', 'Marshall', 'Dimitar', 'Charmander']
     res.render('friends', {friends: friends});
 })
 
