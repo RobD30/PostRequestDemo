f4f7a0e56e3749299459f5d6e4974f4bd12701e7
RobD30 <rob.dickerson.dev@gmail.com>
2018-05-31 18:10:48 +0000

    Created amanda, friends, and routes

--- app.js
+++ app.js
@@ -7,8 +7,18 @@ app.get('/', function(req, res){
     res.render('home');
 })
 
+app.get('/friends', function(req, res){
+    res.render('friends');
+})
+
 app.get('/holley', function(req, res){
     res.render('holley');
+    console.log('Someone looked at Holley\'s page')
+})
+
+app.get('/amanda', function(req, res){
+    res.render('amanda')
+    console.log('Someone looked at Amanada\'s page')
 })
 
 app.listen(process.env.PORT, process.env.IP, function(){
